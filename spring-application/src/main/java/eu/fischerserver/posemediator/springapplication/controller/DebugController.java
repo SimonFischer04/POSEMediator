61d262707c54e1fb34c886eaad2cc00dbf7930bd
Simon Fischer <simi.fischa@gmail.com>
2023-02-19 23:42:15 +0100

    add test controller method for mapping dto->entity

--- spring-application/src/main/java/eu/fischerserver/posemediator/springapplication/controller/DebugController.java
+++ spring-application/src/main/java/eu/fischerserver/posemediator/springapplication/controller/DebugController.java
@@ -4,8 +4,11 @@ import eu.fischerserver.posemediator.springapplication.model.Credential;
 import eu.fischerserver.posemediator.springapplication.service.CredentialService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/debug")
@@ -22,6 +25,12 @@ public class DebugController {
     @GetMapping("test2")
     public Credential test2() {
         return credentialService.findByKey("test");
+    }
+
+    @PutMapping("test3")
+    public Credential test3() {
+        var cred = credentialService.findByKey("test");
+        return credentialService.save(new Credential(cred.id(), cred.key(), UUID.randomUUID().toString()));
     }
 
     public record Test(String a) {
