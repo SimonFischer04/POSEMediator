a2d25b42c437b111de51e5f438d2ab1c5a941849
Simon Fischer <simi.fischa@gmail.com>
2023-02-19 23:58:21 +0100

    add sample external converter

--- spring-application/src/main/java/eu/fischerserver/posemediator/springapplication/config/MapperSpringConfig.java
+++ spring-application/src/main/java/eu/fischerserver/posemediator/springapplication/config/MapperSpringConfig.java
@@ -2,10 +2,18 @@ package eu.fischerserver.posemediator.springapplication.config;
 
 import org.mapstruct.MapperConfig;
 import org.mapstruct.ReportingPolicy;
+import org.mapstruct.extensions.spring.ExternalConversion;
 import org.mapstruct.extensions.spring.SpringMapperConfig;
 import org.mapstruct.extensions.spring.pm.adapter.MyAdapter;
 
+import java.util.Locale;
+
 @MapperConfig(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = MyAdapter.class)
-@SpringMapperConfig(conversionServiceAdapterPackage = "org.mapstruct.extensions.spring.pm.adapter", conversionServiceAdapterClassName = "MyAdapter")
+@SpringMapperConfig(
+        conversionServiceAdapterPackage = "org.mapstruct.extensions.spring.pm.adapter",
+        conversionServiceAdapterClassName = "MyAdapter",
+        // Use Spring included converters: https://github.com/spring-projects/spring-framework/tree/main/spring-core/src/main/java/org/springframework/core/convert/support
+        externalConversions = @ExternalConversion(sourceType = String.class, targetType = Locale.class)
+)
 public interface MapperSpringConfig {
 }
